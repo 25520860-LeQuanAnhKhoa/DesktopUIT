a3fb9ae5d422804fc7a6b0575c8fe4943006a337
25521622 <25521622@gm.uit.edu.vn>
2026-04-30 13:43:18 +0700

    Create 25521622.cpp

---
+++ 25521622.cpp
@@ -0,0 +1,74 @@
+// Nguyen Tan Tai
+// Chuc nang: Chinh sua UI (block vuong + vien)
+
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+#define H 20
+#define W 15
+
+char board[H][W];
+
+// Block mau (test UI)
+char blocks[1][4][4] = {
+    {
+        {' ','O','O',' '},
+        {' ','O','O',' '},
+        {' ',' ',' ',' '},
+        {' ',' ',' ',' '}
+    }
+};
+
+int x = 5, y = 5;
+
+void gotoxy(int x, int y) {
+    COORD c = {x, y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
+
+void initBoard(){
+    for (int i = 0 ; i < H ; i++)
+        for (int j = 0 ; j < W ; j++)
+            if (i == H-1 || j == 0 || j == W-1)
+                board[i][j] = '#';
+            else
+                board[i][j] = ' ';
+}
+
+void draw(){
+    gotoxy(0,0);
+    for (int i = 0 ; i < H ; i++){
+        for (int j = 0 ; j < W ; j++){
+            if (board[i][j] == '#'){
+                cout << "[]";   // viền
+            }
+            else if (board[i][j] == ' '){
+                cout << "  ";   // ô trống
+            }
+            else{
+                cout << "██";   // block vuông
+            }
+        }
+        cout << endl;
+    }
+}
+
+void block2Board(){
+    for (int i = 0 ; i < 4 ; i++)
+        for (int j = 0 ; j < 4 ; j++)
+            if (blocks[0][i][j] != ' ')
+                board[y+i][x+j] = blocks[0][i][j];
+}
+
+int main(){
+    system("cls");
+    initBoard();
+
+    while (1){
+        block2Board();
+        draw();
+        Sleep(200);
+    }
+}
